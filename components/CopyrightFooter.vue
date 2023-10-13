f49ed6febab8e8c05369a62147d1394365bad0b4
Oktay <ojazideh1@gmail.com>
2023-10-13 14:21:26 +0330

    oktayjazideh

---
+++ components/CopyrightFooter.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="text-center fixed w-full lg:w-2/3 bottom-0 right-0 text-gray-500 bg-white py-2">
+		made with <span class="text-primary">❤</span> by
+		<a href="https://ejad.net" target="_blank" class="transition-all font-bold text-dark hover:text-secondary">Ejad Solutions</a>
+	</div>
+</template>
